6374410a9c531efafa3fd744380063bfe4de2a96
JoaoPaisana <joaopaisana@hotmail.com>
2020-01-07 15:26:46 +0000

    change handler input

--- src/App.js
+++ src/App.js
@@ -25,14 +25,19 @@ class App extends Component {
     } )
   }
 
-  nameChangedHandler = ( event ) => {
-    this.setState( {
-      persons: [
-        { name: 'Max', age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: 'Stephanie', age: 26 }
-      ]
-    } )
+  nameChangedHandler = ( event, id ) => {
+
+    const personIndex = this.state.persons.findIndex(p =>{
+      return p.id === id;
+    });
+    
+    const person = {...this.state.persons[personIndex]};
+
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+
+    persons[personIndex] = person;
+    this.setState( {persons: persons} );
   }
 
   togglePersonsHandler = () => {
@@ -66,7 +71,7 @@ class App extends Component {
               name={person.name}
               age={person.age}
               key={person.id}
-              // changed={this.nameChangedHandler}
+              changed={(event)=>this.nameChangedHandler(event, person.id)}
             />
       })}
           {/* <Person
